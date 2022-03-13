037e516bbf621a4b9d1f81a142dd2fa7af308af8
yohannesakd <yoniassefayoni@gmail.com>
2022-03-13 10:49:38 +0300

    merge jrobi's branch

--- app.js
+++ app.js
@@ -3,15 +3,37 @@ const headerMenuHamburger = document.querySelector(".headerMenuHamburger");
 const primaryNav = document.querySelector(".primaryNav");
 const headerSignUp = document.querySelector(".headerSignUp");
 
+const myNav = document.querySelector("header");
+//const logoFirst = document.querySelector(".logo > svg .cls-1");
+
+window.onscroll = function() {
+    if (
+        document.body.scrollTop >= window.innerHeight ||
+        document.documentElement.scrollTop >= window.innerHeight
+    ) {
+        myNav.classList.add("nav-colored");
+        myNav.classList.add("transition");
+        myNav.classList.remove("nav-transparent");
+        // logoFirst.style.fill = "white";
+    } else {
+        myNav.classList.add("nav-transparent");
+        myNav.classList.remove("nav-colored");
+
+        // logoFirst.style.fill = "black";
+    }
+};
+
 let menuOpen = false;
+
 function toggleMenu() {
-  headerMenuHamburger.classList.toggle("open");
-  primaryNav.classList.toggle("open");
-  headerSignUp.classList.toggle("hidden");
-  menuOpen = !menuOpen;
+    headerMenuHamburger.classList.toggle("open");
+    primaryNav.classList.toggle("open");
+    headerSignUp.classList.toggle("hidden");
+    menuOpen = !menuOpen;
 }
+
 function checkResize() {
-  if (menuOpen && window.innerWidth >= "1024") toggleMenu();
+    if (menuOpen && window.innerWidth >= "1024") toggleMenu();
 }
 window.addEventListener("resize", checkResize);
 headerMenu.addEventListener("click", toggleMenu);
